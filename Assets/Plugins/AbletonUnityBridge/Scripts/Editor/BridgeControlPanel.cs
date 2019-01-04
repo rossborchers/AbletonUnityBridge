0fb7546fd6a42c97fbc98f8f190280081b7c4f66
Ross Borchers <rossborchers@gmail.com>
2019-01-05 00:22:15 +0200

    export test (failed)

---
+++ Assets/Plugins/AbletonUnityBridge/Scripts/Editor/BridgeControlPanel.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEditor;
+using UnityEngine;
+
+public class BridgeControlPanel : EditorWindow
+{
+	[MenuItem("Ableton/Bridge Control Panel")]
+	public static void ShowWindow()
+	{
+		//Show existing window instance. If one doesn't exist, make one.
+		EditorWindow.GetWindow(typeof(BridgeControlPanel));
+	}
+
+	void OnGUI()
+	{
+		AbletonLink.Instance.Enable(GUILayout.Toggle(AbletonLink.Instance.IsEnabled(), "Enabled"));
+		GUILayout.Label("Enabled: " + AbletonLink.Instance.IsEnabled(), EditorStyles.boldLabel);
+		GUILayout.Label("Peers: " + AbletonLink.Instance.NumPeers(), EditorStyles.boldLabel);
+	}
+}
--- Plugin/Src/AbletonUnityBridge.h
+++ Plugin/Src/AbletonUnityBridge.h
@@ -2,7 +2,6 @@
 #ifndef ABLETON_UNITY_BRIDGE_H
 #define ABLETON_UNITY_BRIDGE_H
 
-
 #if defined(WIN32) || defined(_WIN32)
 #define UNITY_INTERFACE_EXPORT __declspec(dllexport)
 #define UNITY_INTERFACE_API __stdcall
@@ -15,17 +14,17 @@ extern "C"
 {
 	UNITY_INTERFACE_EXPORT void* UNITY_INTERFACE_API CreateAbletonLink();
 	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API DestroyAbletonLink(void* ptr);
-	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API setup(void* ptr, double bpm);
-	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API setTempo(void* ptr, double bpm);
-	UNITY_INTERFACE_EXPORT double UNITY_INTERFACE_API tempo(void* ptr);
-	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API setQuantum(void* ptr, double quantum);
-	UNITY_INTERFACE_EXPORT double UNITY_INTERFACE_API quantum(void* ptr);
-	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API forceBeatAtTime(void* ptr, double beat);
-	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API requestBeatAtTime(void* ptr, double beat);
-	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API enable(void* ptr, bool bEnable);
-	UNITY_INTERFACE_EXPORT bool UNITY_INTERFACE_API isEnabled(void* ptr);
-	UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API numPeers(void* ptr);
-	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API update(void* ptr, double* rbeat, double* rphase, double* rtempo, double* rtime, int* rnumPeers);
+	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API Setup(void* ptr, double bpm);
+	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API SetTempo(void* ptr, double bpm);
+	UNITY_INTERFACE_EXPORT double UNITY_INTERFACE_API Tempo(void* ptr);
+	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API SetQuantum(void* ptr, double quantum);
+	UNITY_INTERFACE_EXPORT double UNITY_INTERFACE_API Quantum(void* ptr);
+	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API ForceBeatAtTime(void* ptr, double beat);
+	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API RequestBeatAtTime(void* ptr, double beat);
+	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API Enable(void* ptr, bool bEnable);
+	UNITY_INTERFACE_EXPORT bool UNITY_INTERFACE_API IsEnabled(void* ptr);
+	UNITY_INTERFACE_EXPORT int UNITY_INTERFACE_API NumPeers(void* ptr);
+	UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API Update(void* ptr, double* rbeat, double* rphase, double* rtempo, double* rtime, int* rnumPeers);
 	//UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API setNumPeersCallback(void* ptr, numPeersCallback func);
 	//UNITY_INTERFACE_EXPORT void UNITY_INTERFACE_API setTempoCallback(void* ptr, tempoCallback func);
 }
